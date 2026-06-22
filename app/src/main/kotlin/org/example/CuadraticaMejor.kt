8a53175a35233054ff59fc360e9678dae8f29b9f
Mackinnon} <l.mackinnon@alumno.etec.um.edu.ar>
2026-06-22 08:21:20 -0300

    22/06/2026-01

--- app/src/main/kotlin/org/example/CuadraticaMejor.kt
+++ app/src/main/kotlin/org/example/CuadraticaMejor.kt
@@ -1,5 +1,7 @@
 package org.example
 
 class CuadraticaMejor: Cuadratica() {
-
+    override fun toString(): String {
+        return "soy una cuadratica con aura"
+    }
 }
---
+++ app/src/main/kotlin/org/example/Cuatrinomio.kt
@@ -0,0 +1,7 @@
+package org.example
+
+open class Cuatrinomio(): Polinomio() {
+    override fun toString(): String {
+        return "soy un cuatrinomio"
+    }
+}
